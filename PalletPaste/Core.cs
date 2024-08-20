5412a37a0622e9ebe3ecda3ed1498632ac9a05cb
Fizzy <21183059+Fizzyhex@users.noreply.github.com>
2024-08-20 01:45:15 +0100

    Added assembly attributes

--- PalletPaste/Core.cs
+++ PalletPaste/Core.cs
@@ -2,6 +2,9 @@
 using UnityEngine;
 
 [assembly: MelonInfo(typeof(PalletPaste.Core), "PalletPaste", "1.0.0", "Fizzyhex", null)]
+[assembly: MelonColor(20, 194, 215, 255)]
+[assembly: MelonAuthorColor(108, 116, 230, 255)]
+[assembly: MelonAdditionalDependencies("LabFusion")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 
 namespace PalletPaste
